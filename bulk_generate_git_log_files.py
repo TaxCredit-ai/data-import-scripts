40775a19a2063a259f4ebe931f6a87b1787c8248
theoyannekis <33228851+theoyannekis@users.noreply.github.com>
2025-09-04 08:07:20 -0400

    Add instructions

--- bulk_generate_git_log_files.py
+++ bulk_generate_git_log_files.py
@@ -1,3 +1,23 @@
+"""
+Instructions for using this script
+1. Create a csv containing the information on all repositories you with to generate log files for. This csv
+   must have two columns, Organization and Repository. Organization should be the GitHub organization that owns the
+   repository and Repository should be the repository name. An example csv for the repositories
+   https://github.com/django/django-asv and https://github.com/django/code-of-conduct would be:
+   Organization | Repository
+   django, django-asv
+   django, code-of-conduct
+2. If you have not already, set up GitHub authentication on your computer. Instructions can be found here:
+   https://docs.github.com/en/get-started/git-basics/caching-your-github-credentials-in-git
+3. Open a terminal, navigate to the directory containing this script, and run it as follows:
+   python bulk_generate_git_log_files.py {year} {path_to_repository_csv}
+   If you want commits from 2024 and the path to the repository information csv is "repo_names.csv" the command
+   would be python bulk_generate_git_log_files.py 2024 repo_names.csv
+"""
+
+
+
+
 import os
 import csv
 import sys
